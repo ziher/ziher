07c70e90fc31ce7e9b96bc87787f309b48f193c4
perk <marcin.stozek@gmail.com>
2012-03-25 12:18:03 +0200

    Wplywy/wydatki w ksiazkach maja konkretne wartosci

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,13 @@
 class Entry < ActiveRecord::Base
   has_many :items
+
+  def get_amount_for_category(categoryID)
+    result = self.items.find(:first, :conditions=>{:category_id=>categoryID})
+
+    if result != nil
+      return result.amount
+    else
+      return 0
+    end
+  end
 end
