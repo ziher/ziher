dbdbb9ea90fcc66129a9c45f11c75bd9ea76cc99
Andrzej Stencel <andrzej.stencel@gmail.com>
2012-03-25 11:37:10 +0200

    Dodanie autentykacji za pomoca gema 'devise'.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,10 @@ Ziher::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => false
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
