3c02df38d4613e0bbe0450dd7633c2234d9ece21
Andrzej Stencel <andrzej.stencel@gmail.com>
2012-03-25 17:02:05 +0200

    Dodanie użytkownika do seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# The data can then be loaded with the rake db:seed (or create!d alongside the db with db:setup).
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   cities = City.create!([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create!(name: 'Emanuel', city: cities.first)
 
-e1 = Entry.create(name: 'jakies entry', document_number: 'ey1')
-e2 = Entry.create(name: 'jakies drugie entry', document_number: 'ntr2')
+e1 = Entry.create!(name: 'jakies entry', document_number: 'ey1')
+e2 = Entry.create!(name: 'jakies drugie entry', document_number: 'ntr2')
 
-c1 = Category.create(name: 'Darowizny', isExpense: true)
-c2 = Category.create(name: 'Akcje zarobkowe', isExpense: true)
-Category.create(name: 'Wyposazenie', isExpense: false)
-Category.create(name: 'Materialy', isExpense: false)
-Category.create(name: 'Transport', isExpense: false)
-Category.create(name: 'Wynagrodzenia', isExpense: false)
+c1 = Category.create!(name: 'Darowizny', isExpense: true)
+c2 = Category.create!(name: 'Akcje zarobkowe', isExpense: true)
+Category.create!(name: 'Wyposazenie', isExpense: false)
+Category.create!(name: 'Materialy', isExpense: false)
+Category.create!(name: 'Transport', isExpense: false)
+Category.create!(name: 'Wynagrodzenia', isExpense: false)
 
-Item.create(amount: 5, category_id: c1.id, entry_id: e1.id)
-Item.create(amount: 7, category_id: c2.id, entry_id: e2.id)
-Item.create(amount: 3, category_id: c1.id, entry_id: e2.id)
-Item.create(amount: 11, category_id: c2.id, entry_id: e1.id)
+user = User.create!(email: 'ziher2@example.com', password: 'ziherszmicher', confirmed_at: '2012-03-24 22:37:00', confirmation_sent_at: '2012-03-24 22:36:09')
+user.confirm!
+
+Item.create!(amount: 5, category_id: c1.id, entry_id: e1.id)
+Item.create!(amount: 7, category_id: c2.id, entry_id: e2.id)
+Item.create!(amount: 3, category_id: c1.id, entry_id: e2.id)
+Item.create!(amount: 11, category_id: c2.id, entry_id: e1.id)
+
+
