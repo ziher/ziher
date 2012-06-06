a2bf30b69553ff891348a3ce5740dae23c6759c7
perk <marcin.stozek@gmail.com>
2012-06-06 22:30:04 +0200

    Po zalogowaniu się idź od razu do książki finansowej; kilka spolszczeń

--- config/routes.rb
+++ config/routes.rb
@@ -80,7 +80,7 @@ Ziher::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'entries#index'
 
   # See how all your routes lay out with "rake routes"
 
