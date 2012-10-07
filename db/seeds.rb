4e113641484ef2e99a1f408c37f5b6f892bdd021
perk <marcin.stozek@gmail.com>
2012-10-07 12:12:54 +0200

    Laduj troche wiecej danych startowych

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,26 +10,40 @@
 
 c1 = Category.create!(name: 'Darowizny', isExpense: true)
 c2 = Category.create!(name: 'Akcje zarobkowe', isExpense: true)
-Category.create!(name: 'Wyposazenie', isExpense: false)
-Category.create!(name: 'Materialy', isExpense: false)
-Category.create!(name: 'Transport', isExpense: false)
+c3 = Category.create!(name: 'Transport', isExpense: false)
+c4 = Category.create!(name: 'Wyposazenie', isExpense: false)
+c5 = Category.create!(name: 'Materialy', isExpense: false)
 Category.create!(name: 'Wynagrodzenia', isExpense: false)
 
 user = User.create!(email: 'ziher_to@zhr.pl', password: '0xDEADBEEF', confirmed_at: '2012-03-24 22:37:00', confirmation_sent_at: '2012-03-24 22:36:09')
 user.confirm!
 
-e1 = Entry.new(name: 'jakies entry', document_number: 'ey1')
-e2 = Entry.new(name: 'jakies drugie entry', document_number: 'ntr2')
+e1 = Entry.new(name: 'entry 1: darowizna', document_number: 'ey1')
+e2 = Entry.new(name: 'entry 2: akcja, material', document_number: 'ntr2')
+e3 = Entry.new(name: 'entry 3: wyposazenie', document_number: 'fv4')
+e4 = Entry.new(name: 'entry 4: transport', document_number: 'trnsprtdrwn')
+e5 = Entry.new(name: 'entry 5: darowizna, wyposazenie', document_number: 'dar34')
 
-item1 = Item.create!(amount: 5, category_id: c1.id, entry_id: e1.id)
-item2 = Item.create!(amount: 7, category_id: c2.id, entry_id: e2.id)
-item3 = Item.create!(amount: 3, category_id: c1.id, entry_id: e2.id)
-item4 = Item.create!(amount: 11, category_id: c2.id, entry_id: e1.id)
+item1 = Item.create!(amount: 1, category_id: c1.id, entry_id: e1.id)
+item2 = Item.create!(amount: 2, category_id: c2.id, entry_id: e2.id)
+item3 = Item.create!(amount: 3, category_id: c5.id, entry_id: e2.id)
+item4 = Item.create!(amount: 4, category_id: c4.id, entry_id: e3.id)
+item5 = Item.create!(amount: 5, category_id: c3.id, entry_id: e4.id)
+item6 = Item.create!(amount: 6, category_id: c1.id, entry_id: e5.id)
+item7 = Item.create!(amount: 7, category_id: c4.id, entry_id: e5.id)
 
 e1.items << item1
 e2.items << item2
+e2.items << item3
+e3.items << item4
+e4.items << item5
+e5.items << item6
+e5.items << item7
 e1.save!
 e2.save!
+e3.save!
+e4.save!
+e5.save!
 
 JournalType.create!(name: "Książka finansowa")
 JournalType.create!(name: "Książka bankowa")
