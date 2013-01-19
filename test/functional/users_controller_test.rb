4b5f3c25ab6077078fe0c1f24bac36b249dbfaa7
jedr <andrzej.stencel@gmail.com>
2013-01-19 21:12:31 +0100

    Dodanie testów funkcjonalnych sprawdzających, że zwykły użytkownik (nie będący superadminem) nie może zarządzać użytkownikami.

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -2,28 +2,47 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   setup do
-    sign_in users(:admin)
-    @user = users(:admin)
   end
 
   test "should get index" do
+    sign_in users(:admin)
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
   end
 
   test "should show user" do
-    get :show, id: @user.to_param
+    sign_in users(:admin)
+    @admin = users(:admin)
+    get :show, id: @admin.to_param
     assert_response :success
   end
 
   test "should get edit user" do
-    get :edit, id: @user.to_param
+    sign_in users(:admin)
+    @admin = users(:admin)
+    get :edit, id: @admin.to_param
     assert_response :success
   end
 
   test "should get new user" do
+    sign_in users(:admin)
     get :new
     assert_response :success
+  end
+
+  test "should fail getting index" do
+    sign_in users(:user1)
+    assert_raise(CanCan::AccessDenied) {
+      get :index
+    }
+  end
+  
+  test "should fail getting edit user" do
+    sign_in users(:user1)
+    @admin = users(:admin)
+    assert_raise(CanCan::AccessDenied) {
+      get :edit, id: @admin.to_param 
+    }
   end
 end
