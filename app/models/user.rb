ae59691cdea834156caef18678f83465bd380d48
jedr <andrzej.stencel@gmail.com>
2013-01-19 15:12:31 +0100

    Użytkownik będący superadminem może ustawić innym użytkownikom (również sobie), czy są superadminami.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :units
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :is_superadmin, :confirmed_at, :confirmation_sent_at
 
   def status
     if self.confirmed_at == nil
