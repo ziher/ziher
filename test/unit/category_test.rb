20db35e14e313ae5e4fcc09bbc3d00ae616dad27
magdzikk <magda.stozek@gmail.com>
2013-02-23 15:45:00 +0100

    Poprawka testów dla kategorii, tak żeby były bardziej odporne na zmiany

--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -4,11 +4,23 @@ class CategoryTest < ActiveSupport::TestCase
   fixtures :categories
 
   test "should count all years" do
-    assert_equal 3, Category.get_all_years().count
+    years = []
+    Category.all.each do |category|
+      years << category.year
+    end
+
+    assert_equal years.uniq.count, Category.get_all_years().count
   end
 
   test "should sort all years" do
-    assert_equal [2012, 2013, 2014], Category.get_all_years()
+    years = Category.get_all_years()
+
+    years.each_with_index do |year, index|
+      if index+1 < years.count
+        next_year = years[index+1]
+        assert year < next_year
+      end
+    end
   end
 
   test "should find categories for year and type" do
