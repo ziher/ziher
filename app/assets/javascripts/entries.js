5e5cff23d423e9730cdd6f87d28badb2c5537a5b
magdzikk <magda.stozek@gmail.com>
2013-05-01 00:10:26 +0200

    [#40067971] Pierwszy szkic przenoszenia kwot między książkami

---
+++ app/assets/javascripts/entries.js
@@ -0,0 +1,27 @@
+function journalChanged(){
+  hideAll();
+  showJournalsSelect();
+  showSelectedJournal();
+}
+
+function transferChanged(input){
+  hideAll();
+  if(input.checked){
+    showJournalsSelect();
+    showSelectedJournal();
+  }
+}
+
+function hideAll(){
+  jQuery("#journals").hide();
+  jQuery(".journal-categories").hide();
+}
+
+function showSelectedJournal(){
+  var selectedJournal = jQuery("select#journals option:selected").val();
+  jQuery("#journal-" + selectedJournal).show();
+}
+
+function showJournalsSelect(){
+  jQuery("#journals").show();
+}
