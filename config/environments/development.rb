5d45fe44e05dbd47e768c84dd4c8beac91688d83
perk <marcin.stozek@gmail.com>
2013-08-20 01:24:15 +0200

    Poprawki po aktualizacji twitter-bootstrap-rails: rails g bootstrap:install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,5 +32,7 @@ Ziher::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+  config.assets.compile = true
+
   config.i18n.available_locales = :pl
 end
