f81cf8f8d41ac586deaceeca4f6d2d72a321c5ef
perk <marcin.stozek@gmail.com>
2013-09-12 20:25:16 +0000

    Aktualizacja Railsów do 3.2.14

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,11 @@ Ziher::Application.configure do
   config.assets.compile = true
 
   config.i18n.available_locales = :pl
+
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
