fe7bbd76c58e0d2af8a230084389ab61112087fd
perk <marcin.stozek@gmail.com>
2013-11-29 23:47:02 +0100

    Funkcja menu_active? przyjmuje symbole i potrafi zaznaczyć 'Administrację'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,42 @@
 module ApplicationHelper
   def menu_active?(page_name)
-    "active" if current_page?(page_name)
+    case page_name
+    when :journal_finance
+      "active" if false
+
+    when :journal_bank
+      "active" if false
+
+    when :journal_inventory
+      "active" if current_page?(inventory_entries_path)
+
+    when :units
+      "active" if current_page?(units_path)
+
+    when :groups
+      "active" if current_page?(groups_path)
+
+    when :categories
+      "active" if current_page?(categories_path)
+
+    when :journals
+      "active" if current_page?(journals_path)
+
+    when :journal_types
+      "active" if current_page?(journal_types_path)
+
+    when :users
+      "active" if current_page?(users_path)
+
+    when :administration_dropdown
+      "active" if
+        current_page?(units_path) or
+        current_page?(groups_path) or
+        current_page?(categories_path) or
+        current_page?(journals_path) or
+        current_page?(journal_types_path) or
+        current_page?(users_path)
+
+    end
   end
 end
