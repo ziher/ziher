03d8633299699746ad433f2a856ac820d41fb9af
jedr <andrzej.stencel@gmail.com>
2014-01-06 22:51:40 +0000

    I jeszcze plik schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130925200923) do
+ActiveRecord::Schema.define(:version => 20140106210952) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -152,6 +152,9 @@ ActiveRecord::Schema.define(:version => 20130925200923) do
     t.string   "invited_by_type"
     t.boolean  "is_superadmin",                        :default => false
     t.boolean  "is_blocked",                           :default => false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
