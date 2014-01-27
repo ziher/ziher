213704aff161dae8c2ab4fa039263fe1472f8709
perk <marcin.stozek@gmail.com>
2014-01-27 23:44:06 +0100

    Wpisy w książce inwentarzowej mają teraz pole 'Wartość' zamiast 'Cena jednostkowa' - jest to spowodowane tym, że czasem cena jednostkowa wychodzi niewymierna (gdy np. dostaliśmy rabat na namioty i 3 sztuki kupiliśmy za 1000 pln zamiast 1200 pln). Zonk.

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(:version => 20140106210952) do
     t.string   "document_number"
     t.string   "source"
     t.integer  "amount"
-    t.decimal  "unit_price",      :precision => 9, :scale => 2
+    t.decimal  "total_value",     :precision => 9, :scale => 2
     t.string   "comment"
     t.datetime "created_at",                                    :null => false
     t.datetime "updated_at",                                    :null => false
