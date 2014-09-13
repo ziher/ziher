b1faee891813ac633e2ce767af95510c80953efb
perk <marcin.stozek@gmail.com>
2014-09-13 20:09:50 +0200

    Aktualizacja Rubiego 1.9.3 -> 2.1.2

--- bootstrap-vagrant.sh
+++ bootstrap-vagrant.sh
@@ -16,12 +16,11 @@ DEBIAN_FRONTEND=noninteractive apt-get update
 
 echo ====================== Instaluje RVM
 DEBIAN_FRONTEND=noninteractive apt-get --yes install git curl vim
-# there is a problem with twitter-bootstrap-rails and ruby 2.0 so let's use 1.9.3 instead
-sudo -H -u vagrant -i bash -c "curl -L https://get.rvm.io | bash -s stable --ruby=1.9.3"
+sudo -H -u vagrant -i bash -c "curl -L https://get.rvm.io | bash -s stable --ruby=2.1.2"
 sudo -H -u vagrant -i echo "source /home/vagrant/.rvm/scripts/rvm" >> /home/vagrant/.bashrc
 
 # some ruby variables to make ZiHeR a wee bit faster
-sudo -H -u vagrant -i echo "export RUBY_HEAP_MIN_SLOTS=800000" >> /home/vagrant/.bashrc
+sudo -H -u vagrant -i echo "export RUBY_GC_HEAP_INIT_SLOTS=800000" >> /home/vagrant/.bashrc
 sudo -H -u vagrant -i echo "export RUBY_HEAP_FREE_MIN=100000" >> /home/vagrant/.bashrc
 sudo -H -u vagrant -i echo "export RUBY_HEAP_SLOTS_INCREMENT=300000" >> /home/vagrant/.bashrc
 sudo -H -u vagrant -i echo "export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1" >> /home/vagrant/.bashrc
