8779edf2cb4c91fb6074344506f5ea7711a42b62
perk <marcin.stozek@gmail.com>
2015-09-08 12:34:35 +0200

    Dodane eytkiety z nazwami kategorii do poszczególnych pól w widoku książki

--- app/helpers/journals_helper.rb
+++ app/helpers/journals_helper.rb
@@ -3,13 +3,13 @@ module JournalsHelper
     concat (amount == 0) ? "-" : number_with_precision(amount, :precision => 2).to_s
   end
 
-  def print_formatted_amount_with_one_percent(amount, amount_one_percent)
+  def print_formatted_amount_with_one_percent(category_name, amount, amount_one_percent)
     if amount == 0
         result = "-"
     else
       result = "" + number_with_precision(amount, :precision => 2).to_s
 
-      result += "<br/><small><span title='" + I18n.t('helpers.label.journal.one_percent_tooltip')  + "' class='muted'>"
+      result += "<br/><small><span title='" + category_name + "&#013;" + I18n.t('helpers.label.journal.one_percent_tooltip')  + "' class='muted'>"
       if amount_one_percent != 0
         result += number_with_precision(amount_one_percent, :precision => 2).to_s
       end
