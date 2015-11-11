0395628a0ced31270b63420eadfbf15592d241d9
perk <marcin.stozek@gmail.com>
2015-11-11 19:48:48 +0100

    Usunięcie domyślnego użytkownika, aby przypadkiem go nie włączyć

--- db/import-seeds.rb
+++ db/import-seeds.rb
@@ -8,8 +8,5 @@
 #   cities = City.create!([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create!(name: 'Emanuel', city: cities.first)
 
-user = User.create!(email: 'ziher_to@zhr.pl', password: '0xDEADBEEF', confirmed_at: '2012-03-24 22:37:00', confirmation_sent_at: '2012-03-24 22:36:09', is_superadmin: true)
-user.confirm!
-
 finance = JournalType.create!(id: JournalType::FINANCE_TYPE_ID, name: "Książka finansowa", is_default: true)
 bank = JournalType.create!(id: JournalType::BANK_TYPE_ID, name: "Książka bankowa")
