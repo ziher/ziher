3c64d5e70f75c4db08cb2851d902b4324be2da7b
perk <marcin.stozek@gmail.com>
2017-02-21 10:22:48 +0100

    Fixes #66: Devise podczas aktualizacji konta przyjmuje do zmiany także: email, imię, nazwisko oraz telefon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,17 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, :alert => exception.message
   end
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   helper_method :func
 
   ZIHER_VERSION = VERSION[0].to_s + "." + VERSION[1].to_s + "." + VERSION[2].to_s
   ZIHER_COMMIT = VERSION[3].to_s
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :last_name, :phone])
+  end
+
 end
