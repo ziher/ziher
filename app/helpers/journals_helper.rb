e6e5cc5213e88fb47db5ef53bd5b7e8e25b4625a
perk <marcin.stozek@gmail.com>
2017-03-01 09:57:03 +0100

    Poprawki po aktualizacji Bootstrapa: muted -> text-muted

--- app/helpers/journals_helper.rb
+++ app/helpers/journals_helper.rb
@@ -9,7 +9,7 @@ module JournalsHelper
     else
       result = "" + number_with_precision(amount, :precision => 2).to_s
 
-      result += "<br/><small><span title='" + category_name + "&#013;" + I18n.t('helpers.label.journal.one_percent_tooltip')  + "' class='muted'>"
+      result += "<br/><small><span title='" + category_name + "&#013;" + I18n.t('helpers.label.journal.one_percent_tooltip')  + "' class='text-muted'>"
       if amount_one_percent != 0
         result += number_with_precision(amount_one_percent, :precision => 2).to_s
       end
