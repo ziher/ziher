a4549723e5f34a80a44207e972b40c261086d2ef
perk <marcin.stozek@gmail.com>
2017-03-03 22:02:25 +0100

    Datepicker daje do wyboru tylko daty poprawne dla danej książki (z danego roku) #71

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -6,3 +6,9 @@ function showOrHideLinkedEntry(){
     jQuery("#linked_entry_div").hide();
   }
 }
+
+$(function () {
+    year = $('#journal-year').data('source');
+    $.datepicker.setDefaults({ minDate: new Date(year, 1 - 1, 1) });
+    $.datepicker.setDefaults({ maxDate: new Date(year, 12 - 1, 31) });
+});
