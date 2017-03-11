83c46612cc352973560a3ccff470b7f542613ecf
Magda Stozek <magda.stozek@gmail.com>
2017-03-11 22:10:43 +0100

    Poprawione wyświetlanie liczb w dzienniku zmian

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -4,4 +4,16 @@ class Audit
   scope :newest_first, -> do
     reorder("created_at DESC")
   end
+
+  def formatted_changes
+    pretty_hash = Hash.new
+    audited_changes.each do |key, value|
+      if value.class == Array
+        pretty_hash[key] = '[' + value.join(', ') + ']'
+      else
+        pretty_hash[key] = value.to_s
+      end
+    end
+    pretty_hash
+  end
 end
