758ac2c9be9aaa03265361afc1f8bd6db78e8ae5
perk <marcin.stozek@gmail.com>
2018-08-24 15:29:31 +0200

    Aktualizacja środowiska deweloperskiego (Vagranta) do Ubuntu Bionic 18.04

--- bootstrap-vagrant.sh
+++ bootstrap-vagrant.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 echo ====================== Przygowotuje maszyne...
 sudo -H -u vagrant -i echo "export LANGUAGE=en_US.UTF-8" >> /etc/bash.bashrc
 sudo -H -u vagrant -i echo "export LANG=en_US.UTF-8" >> /etc/bash.bashrc
@@ -10,22 +12,15 @@ update-locale LANG=en_US.UTF-8
 update-locale LC_ALL=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-#dpkg-reconfigure locales
 
-echo ====================== Instaluje Docker
-apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
-DEBIAN_FRONTEND=noninteractive apt-get update
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
 
-DEBIAN_FRONTEND=noninteractive apt-get --yes install docker-engine
+apt-get install --yes docker.io docker-compose
 usermod -aG docker vagrant
 
-curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-
 echo ====================== Instaluje RVM
-DEBIAN_FRONTEND=noninteractive apt-get --yes install git curl vim
+apt-get install --yes git curl vim
 sudo -H -u vagrant -i bash -c "gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
 sudo -H -u vagrant -i bash -c "curl -L https://get.rvm.io | bash -s stable --ruby=2.5.1"
 sudo -H -u vagrant -i echo "source /home/vagrant/.rvm/scripts/rvm" >> /home/vagrant/.bashrc
@@ -42,32 +37,28 @@ sudo -H -u vagrant -i echo "force_color_prompt=yes" >> /home/vagrant/.bashrc
 sudo -H -u vagrant -i echo "source ~/.bashrc" >> /home/vagrant/.bash_profile
 
 echo ====================== Instaluje PostgreSQL
-DEBIAN_FRONTEND=noninteractive apt-get --yes install libpq-dev postgresql postgresql-client
-sudo -u postgres psql postgres -c "alter user postgres with password 'postgres'"
-sudo -u postgres psql postgres -c "create user ziher with password 'ziher' createdb"
-sudo -u postgres sed -i -e 's/\(^local\s*all\s*all\s*\)\(peer$\)/\1md5/g' /etc/postgresql/9.3/main/pg_hba.conf
-service postgresql restart
+docker-compose -f /ziher/docker/docker-compose.yml up -d postgres
+apt-get install --yes libpq-dev
+docker exec -u postgres postgres bash -c "psql postgres -c \"create user ziher with password 'ziher' createdb\""
 
-#echo ====================== Sciagam kod zrodlowy ZiHeR-a
-# Tutaj mamy problem jajka i kury - co ma byc najpierw, kod wyciagniety z gita, czy git?
-# Na razie zakladam, ze uzytkownik sciagnal sobie kod wczesniej i z katalogu glownego odpala Vagranta.
-#sudo -H -u vagrant -i git clone https://github.com/zhr/ziher.git /vagrant/
 sudo -H -u vagrant -i bash -c "git config --global color.ui true"
 
 echo ====================== Sciagam gemy i uzupelniam baze danych
-DEBIAN_FRONTEND=noninteractive apt-get --yes install g++
+apt-get install --yes g++
+# pakiety potrzebne dla wkhtmltopdf
+apt-get install --yes libfontconfig1 libxrender1
 sudo -H -u vagrant -i bash -c "gem install bundle"
-sudo -H -u vagrant -i bash -c "cd /vagrant; bundle install"
-sudo -H -u vagrant -i bash -c "cd /vagrant; rake db:create:all"
-sudo -H -u vagrant -i bash -c "cd /vagrant; rake db:setup"
-sudo -H -u vagrant -i bash -c "cd /vagrant; rake db:migrate RAILS_ENV=test"
+sudo -H -u vagrant -i bash -c "cd /ziher; bundle install"
+sudo -H -u vagrant -i bash -c "cd /ziher; rake db:create:all"
+sudo -H -u vagrant -i bash -c "cd /ziher; rake db:setup"
+sudo -H -u vagrant -i bash -c "cd /ziher; rake db:migrate RAILS_ENV=test"
 
 echo '====================== ZiHeR jest gotowy do zabawy!'
 echo '====================== Aby zalogowac sie na maszyne wpisz'
 echo '====================== $ vagrant ssh <enter>'
 echo '======================'
 echo '====================== Nastepnie wejdz do katalogu z ZiHeRem'
-echo '====================== $ cd /vagrant <enter>'
+echo '====================== $ cd /ziher <enter>'
 echo '======================'
 echo '====================== Po czym odpal serwer http ktory poda Ci ZiHeRa na tacy'
 echo '====================== $ rails server -b 0.0.0.0 <enter>'
