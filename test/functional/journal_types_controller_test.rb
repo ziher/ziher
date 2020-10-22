916a8041a4a733508797905c0fd88058555e657f
jedr <andrzej.stencel@gmail.com>
2020-10-22 20:44:57 +0000

    Poprawki w journal_types_controller_test.rb

--- test/functional/journal_types_controller_test.rb
+++ test/functional/journal_types_controller_test.rb
@@ -1,48 +1,48 @@
 require 'test_helper'
 
-class JournalTypesControllerTest < ActionController::TestCase
+class JournalTypesControllerTest < ActionDispatch::IntegrationTest
   setup do
     sign_in users(:master_1zgm)
     @journal_type = journal_types(:finance)
   end
 
   test "should get index" do
-    get :index
+    get journal_types_path
     assert_response :success
     assert_not_nil assigns(:journal_types)
   end
 
   test "should get new" do
-    get :new
+    get new_journal_type_path
     assert_response :success
   end
 
   test "should create journal_type" do
     assert_difference('JournalType.count') do
-      post :create, params: {journal_type: @journal_type.attributes}
+      post journal_types_url, params: {journal_type: {name: 'New'}}
     end
 
     assert_redirected_to journal_type_path(assigns(:journal_type))
   end
 
   test "should show journal_type" do
-    get :show, params: {id: @journal_type.to_param}
+    get journal_type_path(@journal_type)
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, params: {id: @journal_type.to_param}
+    get edit_journal_type_path(@journal_type)
     assert_response :success
   end
 
   test "should update journal_type" do
-    put :update, params: {id: @journal_type.to_param, journal_type: @journal_type.attributes}
+    put journal_type_url(@journal_type), params: {journal_type: {name: 'Changed'}}
     assert_redirected_to journal_type_path(assigns(:journal_type))
   end
 
   test "should destroy journal_type" do
     assert_difference('JournalType.count', -1) do
-      delete :destroy, params: {id: @journal_type.to_param}
+      delete journal_type_path(@journal_type)
     end
 
     assert_redirected_to journal_types_path
