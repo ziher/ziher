467b6f2471af471ecb824fd5540dc938fcad88e3
jedr <andrzej.stencel@gmail.com>
2020-10-22 21:01:25 +0000

    Poprawki w users_controller_test.rb

--- test/functional/user_group_associations_controller_test.rb
+++ test/functional/user_group_associations_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserGroupAssociationsControllerTest < ActionController::TestCase
+class UserGroupAssociationsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionController::TestCase
+class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
   end
 
   test "should get index" do
     sign_in users(:admin)
-    get :index
+    get users_path
     assert_response :success
     assert_not_nil assigns(:users)
   end
@@ -14,21 +14,21 @@ class UsersControllerTest < ActionController::TestCase
   test "should show user" do
     sign_in users(:admin)
     @admin = users(:admin)
-    get :show, params: {id: @admin.to_param}
+    get user_path(@admin)
     assert_response :success
   end
 
   test "should get edit user" do
     sign_in users(:admin)
     @admin = users(:admin)
-    get :edit, params: {id: @admin.to_param}
+    get edit_user_path(@admin)
     assert_response :success
   end
 
   test "should fail getting edit user" do
     sign_in users(:master_1zgm)
     @admin = users(:admin)
-    get :edit, params: {id: @admin.to_param}
+    get edit_user_path(@admin)
     assert_unauthorized
   end
 end
