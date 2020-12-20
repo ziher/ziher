6580ee5007663e11a41c401d52bf9ee4515a50a1
perk <marcin.stozek@gmail.com>
2020-12-20 21:37:28 +0100

    Dodaj prefiks RAILS_RELATIVE_URL_ROOT do assetów dla środowisk produkcyjnych

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.prefix = "#{ENV['RAILS_RELATIVE_URL_ROOT']}/assets"
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
