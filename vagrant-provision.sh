04c5c35481d0a9ee29b57ac19bae46750ed67375
perk <marcin.stozek@gmail.com>
2021-09-24 19:29:16 +0200

    Zmiana sewera dla kluczy potrzebnych dla RVM

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -21,7 +21,7 @@ usermod -aG docker vagrant
 
 echo ====================== Instaluje RVM
 apt-get install --yes git curl vim gnupg2
-sudo -H -u vagrant -i bash -c "gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
+sudo -H -u vagrant -i bash -c "gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
 sudo -H -u vagrant -i bash -c "curl -L https://get.rvm.io | bash -s stable --ruby=2.5.7"
 sudo -H -u vagrant -i echo "source /home/vagrant/.rvm/scripts/rvm" >> /home/vagrant/.bashrc
 
