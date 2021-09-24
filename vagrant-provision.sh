fd0b70029c4248fae9d12ec9987006aa8000428b
perk <marcin.stozek@gmail.com>
2021-09-24 21:07:44 +0200

    Poprawka dla Vagranta: nie uruchamiaj 'bundle clean' po instalacji każdego z gemów przy 'bundle install'

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -49,6 +49,7 @@ apt-get install --yes g++
 # pakiety potrzebne dla wkhtmltopdf
 apt-get install --yes libfontconfig1 libxrender1 libjpeg-turbo8
 sudo -H -u vagrant -i bash -c "gem install bundler"
+sudo -H -u vagrant -i bash -c "bundler config --local clean false"
 sudo -H -u vagrant -i bash -c "cd /ziher; bundle install"
 sudo -H -u vagrant -i bash -c "cd /ziher; rake db:create:all"
 sudo -H -u vagrant -i bash -c "cd /ziher; rake db:setup"
