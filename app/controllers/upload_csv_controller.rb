6dbcae7a0c5c5d00c21a11e1304c6341ae5c081a
Maciej Krysztofiak <maciej.krysztofiak@codeclutch.dev>
2022-08-02 22:04:36 +0200

    add upload_csv#upload

--- app/controllers/upload_csv_controller.rb
+++ app/controllers/upload_csv_controller.rb
@@ -4,14 +4,21 @@ require 'set'
 WARNINGS = "warnings"
 ERRORS = "errors"
 
+CHECK = 'Check'
+UPLOAD = 'Upload'
+
+ONE_PERC = '1%'
+JOURNAL_COL_NAME = 'Numer książki'
+INCOMES_SUM_NAME = 'Wpływy razem'
+
 class UploadCsvController < ApplicationController
   def index
   end
 
   def upload
-    if params[:commit] == 'Check'
+    if params[:commit] == CHECK
       check()
-    elsif params[:commit] == 'Upload'
+    elsif params[:commit] == UPLOAD
       upload_file()
     end
   end
@@ -21,22 +28,8 @@ class UploadCsvController < ApplicationController
     @rowarraydisp = open_csv_file()
     prepare_data_and_indexes()
 
-    @journals_hash = {}
-
-    # hash with errors and warnings
-    # {row_num => {warning=>warning_type; error=>error_type}}
-    @errors  = Array.new(@rowarraydisp.length()-1) {{WARNINGS=>0, ERRORS=>0}}
-
-    @rowarraydisp[1..@rowarraydisp.length()].each_with_index do |row, index|
-      journal_id = row[@journal_col_index].to_i
-      
-      # prepare hash for journal_id
-      unless @journals_hash.key?(journal_id)
-        @journals_hash[journal_id] = {:rows=>0, :errors=>0, :warnings=>0}
-      end
-      @journals_hash[journal_id][:rows] += 1
-
       # ------------ errors and warnings finder ------------
+    @rowarraydisp[1..@rowarraydisp.length()].each_with_index do |row, index|
       check_row_and_add_errors(@rowarraydisp[0], row, index)
     end
   end
@@ -46,13 +39,17 @@ class UploadCsvController < ApplicationController
     @rowarraydisp = open_csv_file()
     prepare_data_and_indexes()
     skip_warnings = true # TODO: FROM FORM
-    @rowarraydisp[1..@rowarraydisp.length()].each_with_index do |row|
+    @rowarraydisp[1..@rowarraydisp.length()].each_with_index do |row, index|
     # @rowarraydisp[1..2].each_with_index do |row|
       errors, warnings = check_row(@rowarraydisp[0], row)
-      if errors == 0
-        if skip_warnings or warnings == 0
-          # Create object
-        end
+      if errors == 0 and (skip_warnings or warnings == 0)
+        # Create object
+        entry = create_entry(@rowarraydisp[0], row)
+        entry.save!
+      else
+        @errors[index][ERRORS] += errors
+        @errors[index][WARNINGS] += warnings
+        @errors[index][:doc_number] += row[@docno_index]
       end
     end
   end
@@ -80,7 +77,7 @@ class UploadCsvController < ApplicationController
       type = JournalType.find(journaltype_id)
       
       @journal_col_index += 1
-      @rowarraydisp[0].unshift("Numer książki")
+      @rowarraydisp[0].unshift(JOURNAL_COL_NAME)
       @rowarraydisp[1..-1].each_with_index do |row, index|
       # @rowarraydisp[1..3].each_with_index do |row, index|
 
@@ -108,10 +105,8 @@ class UploadCsvController < ApplicationController
     @docno_index = @description_index + 1
 
     # find last expenses column, next one is income
-    # hashing {journal_id: {rows, errors}}
     @rowarraydisp[0].each_with_index do |col_name, index|
-      logger.debug col_name
-      if (col_name == 'Wpływy razem')
+      if (col_name == INCOMES_SUM_NAME)
         @incomes_sum_index = index
         @expenses_sum_index = @incomes_sum_index + 1
       end
@@ -133,6 +128,23 @@ class UploadCsvController < ApplicationController
     @rowarraydisp[0][@expenses_vals_start_index..-1].each do |expense_category|
       @expenses_categories.append(expense_category)  
     end
+    
+    # hash with journal_id => rows, errors, warnings
+    @journals_hash = {}
+
+    # hash with errors and warnings
+    # {row_num => {warning=>warning_type; error=>error_type}}
+    @errors = Array.new(@rowarraydisp.length()-1) {{WARNINGS=>0, ERRORS=>0, :doc_number=>''}}
+
+    @rowarraydisp[1..@rowarraydisp.length()].each do |row|
+      journal_id = row[@journal_col_index].to_i
+      
+      # prepare hash for journal_id
+      unless @journals_hash.key?(journal_id)
+        @journals_hash[journal_id] = {:rows=>0, :errors=>0, :warnings=>0}
+      end
+      @journals_hash[journal_id][:rows] += 1
+    end
   end    
 
   def check_row_and_add_errors(column_names, row, row_index)
@@ -144,6 +156,7 @@ class UploadCsvController < ApplicationController
     
     @journals_hash[journal_id][:warnings] += warnings
     @errors[row_index][WARNINGS] += warnings
+    return errors, warnings
   end
   
   def check_row(column_names, row)
@@ -204,7 +217,7 @@ class UploadCsvController < ApplicationController
         col_name = column_names[@incomes_vals_start_index + index]
 
         # ERROR: only 1% column could be negative
-        if float_val(income) < 0 && col_name != "1%"
+        if float_val(income) < 0 && col_name != ONE_PERC
           errors += 1
         end
 
@@ -257,13 +270,70 @@ class UploadCsvController < ApplicationController
         warnings += 1
       end
 
-
       # ERROR: none incomes and expenses
-      elsif incomes_sum== 0 && expenses_sum == 0
+      elsif incomes_sum == 0 && expenses_sum == 0
         warnings += 1
-      
       end
     return errors, warnings
+  end
+
+  def create_entry(column_names, row)
+    date = row[@date_index]
+    name = row[@description_index]
+    doc_no = row[@docno_index]
+    journal = Journal.find(row[@journal_col_index])
+    is_expense = nil
+
+    items = []
+    #income1, income2, ...
+    row[@incomes_vals_start_index..@incomes_sum_index-1].each_with_index do |income, index|
+      income_val = float_val(income)
+      income_name = column_names[@incomes_vals_start_index + index]
+
+      income = create_income_item(income_name, journal.year, income_val)
+      if income_val != 0
+        is_expense = false
+        items << income
+      end
+    end
+
+    #expense1, expense1_1%, expense2, expense2_2%, ...
+    column_names[@expenses_vals_start_index..-1].each_slice(2).with_index do |expenses, index|
+      expense_col_index = @expenses_vals_start_index + 2*index
+      expense_val = float_val(row[expense_col_index])
+      expense_one_perc_val = float_val(row[expense_col_index + 1])
+
+      expense = create_expense_item(expenses[0], journal.year, expense_val, expense_one_perc_val)
+      if expense_val > 0
+        is_expense = true
+        items << expense
+      end
+    end
+    
+    entry = Entry.new(:date => date, :name => name, :document_number => doc_no, :journal => journal, :items => items)
+    entry.is_expense = is_expense
+    entries_controller = EntriesController.new
+    entries_controller.create_empty_items(entry, journal.year)
+    return entry
+  end
+
+  def create_income_item(income_name, year, value)
+    ctgs = Category.find_by_year_and_type(year, false) # incomes categories
+    income_ctg = ctgs.detect { |c| c.name == income_name }
+
+    item = Item.new(:amount => value, :category => income_ctg)
+    if income_ctg.name == ONE_PERC
+      item.amount_one_percent = value
+    end
+    return item
+  end
+
+  def create_expense_item(expense_name, year, value, value_one_percent=0)
+    ctgs = Category.find_by_year_and_type(year, true) # expense categories
+    expense_ctg = ctgs.detect { |c| c.name == expense_name }
+    
+    item = Item.new(:amount => value, :category => expense_ctg, :amount_one_percent => value_one_percent)
+    return item
   end
 
   def float_val(val)
