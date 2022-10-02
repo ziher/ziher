a7bc789d95ee8a9f23b3c78474eae2e0f27ef524
perk <marcin.stozek@gmail.com>
2022-10-02 17:59:40 +0200

    fix 2dea520: poprawka dla budowania za pomocą skryptu build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 VERSION=$(./update-version.sh)
 
 # create image ziher-app
-docker build . --tag ziher-app:${VERSION}
+docker build . --build-arg SECRET_KEY_BASE=dummykeybase --tag ziher-app:${VERSION}
 docker tag ziher-app:${VERSION} ziher-app:latest
