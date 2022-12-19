82bdd3552b594312b9234a326685a2af2d301326
perk <marcin.stozek@gmail.com>
2022-12-19 20:55:41 +0100

    chore: dodaj możliwość tworzenia baz danych w środowisku docker/dev po odtworzeniu

--- docker/prepare-dev-containers.sh
+++ docker/prepare-dev-containers.sh
@@ -13,7 +13,7 @@ docker-compose up -d --force-recreate
 
 docker-compose exec -T $DB_CONTAINER bash -c "PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c \"drop database $DB_NAME\""
 docker-compose exec -T $DB_CONTAINER bash -c "PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c \"drop user $DB_USER\""
-docker-compose exec -T $DB_CONTAINER bash -c "PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c \"create user $DB_USER with password '$DB_PASSWORD'\""
+docker-compose exec -T $DB_CONTAINER bash -c "PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c \"create user $DB_USER with password '$DB_PASSWORD' createdb\""
 docker-compose exec -T $DB_CONTAINER bash -c "PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c \"create database $DB_NAME with owner $DB_USER\""
 
 docker exec ziher rake db:migrate
