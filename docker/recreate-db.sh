6ee3aba4b338744ab4ebc953b56d41536d289d79
perk <marcin.stozek@gmail.com>
2024-02-11 13:06:14 +0000

    chore: poprawki dla skryptu do resetu baz danych

--- docker/recreate-db.sh
+++ docker/recreate-db.sh
@@ -7,11 +7,21 @@ readonly PGPASSWORD=postgres
 readonly DB_PASSWORD=ziher
 readonly DB_USER=ziher
 
+# this whole script should be something like: rake db:reset
+
 for ziherenv in development stage; do
   DB_NAME="ziher_${ziherenv}"
 
-  docker compose exec -T $DB_CONTAINER bash -c "PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c \"drop database $DB_NAME\""
-  docker compose exec -T $DB_CONTAINER bash -c "PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c \"drop user $DB_USER\""
-  docker compose exec -T $DB_CONTAINER bash -c "PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c \"create role $DB_USER with CREATEDB SUPERUSER login password '$DB_PASSWORD'\""
-  docker compose exec -T $DB_CONTAINER bash -c "PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c \"create database $DB_NAME with owner $DB_USER\""
+  docker compose --project-directory /ziher/docker/ exec -T $DB_CONTAINER bash -c "PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c \"drop database $DB_NAME\""
+	# rether should be: rake db:drop:all
+done
+
+docker compose --project-directory /ziher/docker/ exec -T $DB_CONTAINER bash -c "PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c \"drop role $DB_USER\""
+docker compose --project-directory /ziher/docker/ exec -T $DB_CONTAINER bash -c "PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c \"create role $DB_USER with CREATEDB SUPERUSER login password '$DB_PASSWORD'\""
+
+for ziherenv in development stage; do
+  DB_NAME="ziher_${ziherenv}"
+
+  docker compose --project-directory /ziher/docker/ exec -T $DB_CONTAINER bash -c "PGPASSWORD=$PGPASSWORD psql -h localhost -U postgres -d postgres -c \"create database $DB_NAME with owner $DB_USER\""
+  # rather should be: rake db:create:all
 done
