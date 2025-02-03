fc998524306d43fcc074bd18ab4465f33b9a9dda
Piotr Kochowicz <p.kochowicz@binarapps.com>
2025-02-03 21:27:00 +0100

    fix: Fixes bug with commas and dots. By a scout rule fixes also amounts for grants and categories (#219)
    * Fixes bug with commas and dots. By a scout rule fixes also amounts for grants and categories
    
    * fix for commas

--- app/models/item_grant.rb
+++ app/models/item_grant.rb
@@ -1,11 +1,15 @@
 class ItemGrant < ApplicationRecord
-    # audited
+  # audited
 
-    belongs_to :item , required: true
-    belongs_to :grant, required: true
+  belongs_to :item , required: true
+  belongs_to :grant, required: true
 
-    # validate there is only one grant type per item
-    validates_uniqueness_of :item_id, scope: [:grant_id]
+  # validate there is only one grant type per item
+  validates_uniqueness_of :item_id, scope: [:grant_id]
 
-    validates :amount, numericality: { other_than: 0, message: "Wartość dla dotacji nie może wynosić zero" }
+  validates :amount, numericality: { other_than: 0, message: "Wartość dla dotacji nie może wynosić zero" }
+
+  def amount=(val)
+    write_attribute :amount, val.to_s.gsub(/[,\s+]/, ',' => '.', '\s+' => '')
+  end
 end
