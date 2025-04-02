17465990a452737854f3657b43b135ef475a3c18
oizzoo <108180322+oizzoo@users.noreply.github.com>
2025-04-02 15:02:13 +0200

    feat ziher#204: dodanie w oknie wpływu / wydatku pola suma

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -7,6 +7,16 @@ function showOrHideLinkedEntry(){
   }
 }
 
+// Uniwersalna funkcja do sumowania kwot
+function updateTotalSum(inputSelector, outputId) {
+    var total = 0;
+    $(inputSelector).each(function() {
+        var value = parseFloat($(this).val()) || 0;
+        total += value;
+    });
+    $(outputId).val(total.toFixed(2));
+}
+
 $(function () {
     var year = $('#journal-year').data('source');
     var journal_blocked_to = $('#journal_blocked_to').val();
@@ -30,5 +40,23 @@ $(function () {
         maxDate: new Date(year, 12 - 1, 31),
         numberOfMonths: 3,
         setCurrentAtPos: 1
+    });
+
+    $('.amount-input').on('input', function() {
+        updateTotalSum('.amount-input', '#total-sum');
+    });
+
+    $('.amount-input-one-percent').on('input', function() {
+        updateTotalSum('.amount-input-one-percent', '#total-sum-one-percent');
+    });
+
+    // Sumowanie dla każdego grantu osobno
+    $('.amount-input-grants').each(function() {
+        var grantId = $(this).siblings('.grant_id').val(); // Pobieramy grant_id z ukrytego pola
+        if (grantId) {
+            $(this).on('input', function() {
+                updateTotalSum('.amount-input-grants.grant-' + grantId, '#total-sum-grant-' + grantId);
+            });
+        }
     });
 });
