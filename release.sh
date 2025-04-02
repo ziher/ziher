0b9f396031d55208eb08abb68c82d8a58ea4d32d
perk <marcin.stozek@gmail.com>
2025-04-02 11:58:08 +0200

    feat: buduj i publikuj obraz bazowy dla ZiHeRa

--- release.sh
+++ release.sh
@@ -1,9 +1,43 @@
 #!/bin/bash
 
+readonly RELEASE_TYPE=${1:-prod}
+
+if [[ "${RELEASE_TYPE}" != "prod" && \
+      "${RELEASE_TYPE}" != "base" && \
+      "${RELEASE_TYPE}" != "all" ]]; then
+    echo "Usage: $0 [prod|base|all]"
+    exit 1
+fi
+
 # update the version
 VERSION=$(./update-version.sh)
 
-docker push ziher/app:${VERSION}
+release_prod() {
+    docker push ziher/app:${VERSION}
 
-docker tag ziher/app:${VERSION} ziher/app:latest
-docker push ziher/app:latest
+    docker tag ziher/app:${VERSION} ziher/app:latest
+    docker push ziher/app:latest
+}
+
+release_base() {
+    docker push ziher/base:${VERSION}
+
+    docker tag ziher/base:${VERSION} ziher/base:latest
+    docker push ziher/base:latest
+}
+
+case "${RELEASE_TYPE}" in
+    prod)
+        echo "Releasing production image..."
+        release_prod
+        ;;
+    base)
+        echo "Releasing base image..."
+        release_base
+        ;;
+    all)
+        echo "Releasing all images..."
+        release_base
+        release_prod
+        ;;
+esac
